636a9c68afd7670ed12091232f648907eaa3416a
Marijn Ophorst <marijn@sensimedia.nl>
2015-06-23 10:00:17 +0200

    test base namespace

--- tests/RewriteTest.php
+++ tests/RewriteTest.php
@@ -6,12 +6,24 @@ class RewriteTest extends PHPUnit_Framework_TestCase
     {
         $mural = new Mural\Autoloader;
         $mural->rewrite('Foo', 'Bar\Foo');
+        $mural->rewrite('', 'Noop\\');
         $foo = new Foo;
         $foobar = new Foo\Bar;
         $baz = new Baz;
         $this->assertEquals('Bar\Foo', get_class($foo));
+        $this->assertTrue($foo instanceof Foo);
+        $this->assertTrue($foo instanceof Bar\Foo);
         $this->assertEquals('Foo\Bar', get_class($foobar));
         $this->assertEquals('Baz', get_class($baz));
+        $dummy = new Dummy;
+    }
+
+    public function testInterface()
+    {
+    }
+
+    public function testTrait()
+    {
     }
 }
 
