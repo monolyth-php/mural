ed175c3e20801e95e5baeb2a5a04264a2fec3caf
Marijn Ophorst <marijn@sensimedia.nl>
2015-06-23 09:29:57 +0200

    test some more

--- tests/RewriteTest.php
+++ tests/RewriteTest.php
@@ -5,9 +5,13 @@ class RewriteTest extends PHPUnit_Framework_TestCase
     public function testRewrite()
     {
         $mural = new Mural\Autoloader;
-        $mural->rewrite('\Foo', '\Bar\Foo');
+        $mural->rewrite('Foo', 'Bar\Foo');
         $foo = new Foo;
+        $foobar = new Foo\Bar;
+        $baz = new Baz;
         $this->assertEquals('Bar\Foo', get_class($foo));
+        $this->assertEquals('Foo\Bar', get_class($foobar));
+        $this->assertEquals('Baz', get_class($baz));
     }
 }
 
